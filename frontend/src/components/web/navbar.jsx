d548e16a3bc9cebec870bef02006361bc095bfd2
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-18 18:45:15 +0200

    up

--- frontend/src/components/web/navbar.jsx
+++ frontend/src/components/web/navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        {menuLinks.map((link) => (
+                        {menuLinks?.map((link) => (
                             <li key={link.name}>
                                 <a href={link.link} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{link.name}</a>
                             </li>
