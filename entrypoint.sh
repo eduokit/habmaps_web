903a86f9748ec612b394cb057c24bf6643df7ec3
alpeza <80060968+alpeza@users.noreply.github.com>
2023-04-18 18:37:38 +0200

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-
 cd /app
-cp build/env.js .
-npx react-inject-env set && http-server -c-1 -a 0.0.0.0 -p 80 -d false
+npx react-inject-env set && cp build/env.js .
+http-server -c-1 -a 0.0.0.0 -p 80 -d false
